9a5c1aaace6b6a609690e95a4103701ac7b7a2ae
shivuppal15 <40572341+shivuppal15@users.noreply.github.com>
2018-07-11 23:51:41 +0800

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }],
+  fullName: {
+    type: String,
+    required: true,
+    minlength: 1
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    requred: true
+  },
+  ic: {
+    type: String,
+    required: true,
+    minlength: 9,
+    maxlength: 9
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+    minlength: 8,
+    maxlength: 8
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  creditRating: {
+    type: Number,
+    required: true
+    value: 0
+  },
+  itemsPawned: [{
+    item: {
+      type: String
+    }
+  }]
+});
