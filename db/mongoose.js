c7ac21f57f97f84beb2ab5052ec330f3d8b30ba3
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2018-07-12 22:26:12 +0800

    added authenticate middleware

---
+++ db/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp').then(() => {
+    console.log('Connected to mongodb');
+}).catch((err) => {
+    console.log({
+        msg: 'Unable to connect to mongodb',
+        err
+    });
+});
+
+module.exports = {
+    mongoose
+};
---
+++ middleware/authenticate.js
@@ -0,0 +1,24 @@
+const {User} = require('../db/models/user');
+
+const authenticate = (req, res, next) => {
+    const token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject('User not found');
+        } else {
+            // Add user data to req
+            req.user = user;
+            req.token = token;
+
+            // Call next() so that program moves forward
+            next();
+        }
+    }).catch((e) => {
+        res.status(401).send(e);
+    });
+};
+
+module.exports = {
+    authenticate
+};
