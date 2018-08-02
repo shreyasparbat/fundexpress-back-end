1b827af5b099ad334a964f70ef4cba87959b71d3
Unknown <shreyasp.2016@sis.smu.edu.sg>
2018-08-02 21:22:01 +0530

    added s3 file saving, testing now

--- utils/digitalOceanSpaces.js
+++ utils/digitalOceanSpaces.js
@@ -1,7 +1,5 @@
 // Library imports
 const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
 
 // Setup S3 endpoint to DigitalOcean Spaces
 const spacesEndpoint = new aws.Endpoint('sgp1.digitaloceanspaces.com');
@@ -9,22 +7,29 @@ const s3 = new aws.S3({
     endpoint: spacesEndpoint
 });
 
-// Upload object to digitalOcean spaces
-const upload = multer({
-    storage: multerS3({
-        s3,
-        bucket: 'fundexpress-api-storage',
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        key (name, file, callback) {
-            console.log('Saving file' + name);
-            callback(null, name);
-        }
-    })
-}).array('upload', 1);
-
 // Save IC image to digitalOcean
 const saveIcImage = (icNumber, icImageFront, icImageBack) => {
+    // Save front image
+    s3.putObject({
+        Body: icImageFront,
+        Bucket: "fundexpress-api-storage",
+        Key: "ic-images/" + icNumber + "-front.png",
+    }, (err) =>  {
+        if (err) {
+            return Promise.reject(err);
+        }
+    });
 
+    // Save back image
+    s3.putObject({
+        Body: icImageBack,
+        Bucket: "fundexpress-api-storage",
+        Key: "ic-images/" + icNumber + "-back.png",
+    }, (err) =>  {
+        if (err) {
+            return Promise.reject(err);
+        }
+    });
 };
 
 // Retrieve IC image from digitalOcean
