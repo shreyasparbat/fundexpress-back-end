4b9447c6286f93dbd3d667efe2018d588b2fb0f3
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2018-08-21 11:03:39 +0800

    fixed req.body erroragain

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,7 +2,7 @@ const {User} = require('../db/models/user');
 
 
 const authenticate = (req, res, next) => {
-    const token = req.body.x-auth;
+    const token = req.body['x-auth'];
 
     // Find user with that token
     User.findByToken(token).then((user) => {
