78fe1752250064e464148d9e1f65afb1b1405635
shivuppal15 <40572341+shivuppal15@users.noreply.github.com>
2018-09-03 19:17:11 +0800

    Add files via upload

--- routes/item.js
+++ routes/item.js
@@ -15,9 +15,39 @@ router.use(authenticate);
 
 
 // POST: pawn new item (create pawn ticket)
+router.post('/item/pawn', (req, res) => {
+  try {
+    let body = _.pick(req.body, [
+        'itemID',
+        'specifiedValue'
+    ]);
+    res.send({
+      'ticketNumber',
+      'dateCreated',
+      'expiryDate',
+      'interestPayable',
+      'offeredValue'
+    })
+  } catch (e) {
+      res.status(500).send(e);
+  }
+});
 
 
 // POST: sell new item (create sell ticket)
-
+router.post('/item/sell', (req, res) => {
+  try {
+    let body = _.pick(req.body, [
+        'itemID'
+    ]);
+    res.send({
+      'ticketNumber',
+      'dateCreated',
+      'offeredValue'
+    })
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 module.exports = router;
