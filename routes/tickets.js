d103b96d17035d05790a3831a2b6e42f75160908
LIM Wan Ting <wtlim.2016@sis.smu.edu.sg>
2018-09-10 23:04:41 +0800

    Added /tickets route

--- routes/tickets.js
+++ routes/tickets.js
@@ -16,4 +16,34 @@ router.post('/', (req, res) => {
 });
 
 
+// POST: retrieve tickets
+router.post('/tickets', (req, res) => {
+    try {
+        var today = moment().startOf('day')
+        //retrieve current pawn tickets
+        var results1 = mongoose.find({ userId: req.user.userId, approval: true, expiry: { $gt: today.toDate() }});
+        console.log(results1)
+        
+        //retrieve pawn tickets pending approval
+        var results2 = mongoose.find({ userId: req.user.userId, approval: false, expiry: { $gt: today.toDate() }});
+        console.log(results2)
+        
+        //retrieve expired pawn tickets
+        var results3 = mongoose.find({ userId: req.user.userId, approval: true, expiry: { $lte: today.toDate() }});
+        console.log(results3)
+        
+        //retrieve sell tickets pending approval
+        var results4 = mongoose.find({ userId: req.user.userId, approval: false});
+        console.log(results4)
+        
+        //retrieve approved sell tickets
+        var results5 = mongoose.find({ userId: req.user.userId, approval: true});
+        console.log(results5)
+        
+    } catch (e) {
+        res.status(400).send(e);
+    }
+  });
+
+
 module.exports = router;
