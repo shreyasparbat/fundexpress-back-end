94069fc578f940aba35ae9b060c3aeba2b931f17
LIM Wan Ting <wtlim.2016@sis.smu.edu.sg>
2018-09-24 21:34:24 +0800

    Created authenticateAdmin

---
+++ middleware/authenticateAdmin.js
@@ -0,0 +1,27 @@
+const {Admin} = require('../db/models/admin');
+
+
+const authenticate = (req, res, next) => {
+    const token = req.header('x-auth');
+
+    // Find admin with that token
+    Admin.findByToken(token).then((admin) => {
+        if (!admin) {
+            return Promise.reject('User not found or logged out');
+        } else {
+            // Add admin data to req
+            req.admin = admin;
+            req.token = token;
+
+            // Call next() so that program moves forward
+            next();
+        }
+    }).catch((error) => {
+        console.log(error);
+        res.status(401).send({error});
+    });
+};
+
+module.exports = {
+    authenticate
+};
