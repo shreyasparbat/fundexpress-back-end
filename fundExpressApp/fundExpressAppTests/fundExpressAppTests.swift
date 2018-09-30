057e6ef256d2875b210f2f4aecf355f595ac245e
Shiv <shiv.uppal.2015@sis.smu.edu.sg>
2018-09-30 18:31:22 +0800

    notifications for ios and android

---
+++ fundExpressApp/fundExpressAppTests/fundExpressAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  fundExpressAppTests.swift
+//  fundExpressAppTests
+//
+//  Created by Shiv Uppal on 30/9/18.
+//  Copyright © 2018 Fund Express. All rights reserved.
+//
+
+import XCTest
+@testable import fundExpressApp
+
+class fundExpressAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
