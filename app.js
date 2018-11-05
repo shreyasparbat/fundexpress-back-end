b0ecec5ee85fffe77176df1b659252f59d947c66
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2018-11-05 12:49:58 +0800

    added dotenv

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config();
 
 // Router imports
 const indexRouter = require('./routes/index');
--- routes/adminViews.js
+++ routes/adminViews.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // Page for uploading new CSV for retraining
 router.get('/retrainCreditRatingModel', function(req, res) {
     res.render('retrainCreditRatingModel', { title: 'Upload New CSV' });
+    console.log(req);
 });
 
 
