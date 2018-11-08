2561810fe6e92dc1706ff65d554faff5abede34e
iioviie <40571474+iioviie@users.noreply.github.com>
2018-11-08 18:19:10 +0800

    Updated interest rates for notifs

--- utils/notifications.js
+++ utils/notifications.js
@@ -252,20 +252,21 @@ cron.schedule('0 1 0 * * *', async function () {
     }).lean();
     
     pawnTickets.forEach(ticket => {
+        const currentInterestRate = await InterestRate.find().limit(1).sort({$natural:-1});
         if (new Date().getDate() - ticket.dateCreated.getDate() === 1 && ticket.dateCreated.getMonth() === new Date()          .getMonth()) {
             // adds interest for the first month
-            ticket.outstandingInterest += ticket.value * config.firstMonthRate * 1 / 100;
+            ticket.outstandingInterest += ticket.value * currentInterestRate[0].firstMonthRate * 1 / 100;
 
         } else if (ticket.dateCreated.getDate() === lastday(new Date().getFullYear(), new Date().getMonth()-1) &&               new Date().getMonth() - ticket.dateCreated.getMonth() === 1 && new Date().getDate() === 1) {
             // adds interest for the first month
-            ticket.outstandingInterest += ticket.value * config.firstMonthRate * 1 / 100;
+            ticket.outstandingInterest += ticket.value * currentInterestRate[0].firstMonthRate * 1 / 100;
 
         } else if (ticket.dateCreated.getDate() === new Date().getDate()) {
                 // adds interest for every subsequent month
                 const numMonths = new Date().getMonth() - ticket.dateCreated.getMonth();
 
                 ticket.outstandingInterest = 0;
-                ticket.outstandingInterest += ticket.value * config.normalRate * numMonths / 100;
+                ticket.outstandingInterest += ticket.value * currentInterestRate[0].normalRate * numMonths / 100;
 
         }
     })
